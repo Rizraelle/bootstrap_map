a99a06a665e177056b9b7dd6d6ab78cadaf7e98d
unknown <rizraelleshinobu@gmail.com>
2017-07-03 19:48:23 +0300

    landing page created with bootstrap, contain google map

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+        e.preventDefault();
+        var t = 1000;
+        var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+        $('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+    });
+});
+
+function initMap() {
+        var uluru = {lat: 40.7143528, lng: -74.0059731};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
